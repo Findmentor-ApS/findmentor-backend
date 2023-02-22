523102251dc5bb90f701e9c58d70deedc4390258
samilesma <samilesma@gmail.com>
2023-02-22 12:18:29 +0100

    Søge funktionen i backenden er tilpasset

--- api/controllers/mentors.php
+++ api/controllers/mentors.php
@@ -4,5 +4,17 @@ DI::rest()->get('/mentors/:id', function(RestData $data) {
 });
 
 DI::rest()->get('/mentors', function(RestData $data) {
-  http(200, getMentorsSearch($data->request->getQuery()), true);
-}, ['auth.loggedIn']);
+  $query = [
+    'search' => $data->request->getQuery('search'),
+    'location' => $data->request->getQuery('location'),
+    'typeForm' => $data->request->getQuery('typeForm'),
+    'language' => $data->request->getQuery('language'),
+    'gender' => $data->request->getQuery('gender'),
+    'contact' => $data->request->getQuery('contact'),
+    'target' => $data->request->getQuery('target'),
+  ];
+  $page = $data->request->getQuery('page');
+  $perPage = $data->request->getQuery('perpage');
+  $mentors = getMentorsSearch($query, $page, $perPage);
+  http(200, $mentors, true);
+});
--- api/funcs/funcs.sql.php
+++ api/funcs/funcs.sql.php
@@ -49,8 +49,8 @@
 //     ];
 // }
 
-function getMentorsSearch($query) {
-    $select = "SELECT count(u.id) ";
+function getMentorsSearch($query, $page, $perPage) {
+    $select = "SELECT count(u.id) AS total_count ";
     $sql = "FROM users u
     LEFT JOIN users_mentor_types umt ON umt.user_id = u.id
     LEFT JOIN mentor_types mt ON mt.id = umt.mentor_type_id
@@ -59,14 +59,14 @@ function getMentorsSearch($query) {
     WHERE u.usertype = 'mentor'";
 
     $vals = [];
-    if (isset($query['id'])) {
-        $sql .= " AND u.id = ?";
-        $vals[] = $query['id'];
-    }
+    $scoreQuery = '';
+    $scoreVals = [];
 
     if (isset($query['gender'])) {
         $sql .= " AND u.gender = ?";
         $vals[] = $query['gender'];
+        $scoreQuery .= "(u.gender = ?) * 5 + ";
+        $scoreVals[] = $query['gender'];
     }
 
     if (isset($query['geography'])) {
@@ -75,25 +75,93 @@ function getMentorsSearch($query) {
             foreach ($query['geography'] as $geo) {
                 $sql .= "mg.commune_id = ? OR ";
                 $vals[] = $geo;
+                $scoreQuery .= "(mg.commune_id = ?) * 5 + ";
+                $scoreVals[] = $geo;
             }
             $sql = trim($sql, ' OR ') . ')';
+            $scoreQuery = trim($scoreQuery, ' + ');
         } else {
             $sql .= " AND mg.commune_id = ?";
             $vals[] = $query['geography'];
+            $scoreQuery .= "(mg.commune_id = ?) * 5 + ";
+            $scoreVals[] = $query['geography'];
         }
     }
 
-    $res = R::getAll($select . $sql, $vals);
-    $count = !empty($res) ? $res[0]['count(u.id)'] : 0;
+    if (isset($query['typeForm'])) {
+        if (is_array($query['typeForm'])) {
+            $sql .= " AND (";
+            foreach ($query['typeForm'] as $typeForm) {
+                $sql .= "umt.mentor_type_id = ? OR ";
+                $vals[] = $typeForm;
+                $scoreQuery .= "(umt.mentor_type_id = ?) * 5 + ";
+                $scoreVals[] = $typeForm;
+            }
+            $sql = trim($sql, ' OR ') . ')';
+            $scoreQuery = trim($scoreQuery, ' + ');
+        } else {
+            $sql .= " AND umt.mentor_type_id = ?";
+            $vals[] = $query['typeForm'];
+            $scoreQuery .= "(umt.mentor_type_id = ?) * 5 + ";
+            $scoreVals[] = $query['typeForm'];
+        }
+    }
 
-    if (!isset($query['page']) || !is_numeric($query['page'])) $query['page'] = 0;
-    if (!isset($query['perpage']) || !is_numeric($query['perpage'])) $query['perpage'] = 10;
+    if (isset($query['language'])) {
+        if (is_array($query['language'])) {
+            $sql .= " AND (";
+            foreach ($query['language'] as $language) {
+                $sql .= "u.language = ? OR ";
+                $vals[] = $language;
+                $scoreQuery .= "(u.language = ?) * 5 + ";
+                $scoreVals[] = $language;
+            }
+            $sql = trim($sql, ' OR ') . ')';
+            $scoreQuery = trim($scoreQuery, ' + ');
+        } else {
+            $sql .= " AND u.language = ?";
+            $vals[] = $query['language'];
+            $scoreQuery .= "(u.language = ?) * 5 + ";
+            $scoreVals[] = $query['language'];
+        }
+    }
 
-    $select = "SELECT u.id, u.email, u.firstname, u.lastname, u.phone, GROUP_CONCAT(mt.type) AS mentor_types ";
-    $sql .= " GROUP BY u.id LIMIT " . ($query['page'] * $query['perpage']) . "," . $query['perpage'];
-
+    if (isset($query['search'])) {
+        $searchTerm = $query['search'];
+        $sql .= " AND (u.firstname LIKE ? OR u.lastname LIKE ? OR u.email LIKE ?)";
+        $vals[] = "%$searchTerm%";
+        $vals[] = "%$searchTerm%";
+        $vals[] = "%$searchTerm%";
+        $scoreQuery .= "(u.firstname LIKE ? OR u.lastname LIKE ? OR u.email LIKE ?) * 2 + ";
+        $scoreVals[] = "%$searchTerm%";
+        $scoreVals[] = "%$searchTerm%";
+        $scoreVals[] = "%$searchTerm%";
+    }
+    
+    if (isset($query['contact'])) {
+        $contactMethod = $query['contact'];
+        $sql .= " AND u.contact_method = ?";
+        $vals[] = $contactMethod;
+        $scoreQuery .= "(u.contact_method = ?) * 5 + ";
+        $scoreVals[] = $contactMethod;
+    }
+    
+    if (isset($query['target'])) {
+        $targetAudience = $query['target'];
+        $sql .= " AND u.target_audience = ?";
+        $vals[] = $targetAudience;
+        $scoreQuery .= "(u.target_audience = ?) * 5 + ";
+        $scoreVals[] = $targetAudience;
+    }
+    
+    $sql .= " GROUP BY u.id ORDER BY ({$scoreQuery} 0) DESC LIMIT $perPage OFFSET " . ($page * $perPage);
+    
+    $mentors = R::getAll("SELECT u.id, u.email, u.firstname, u.lastname, u.phone, GROUP_CONCAT(mt.type) AS mentor_types, ($scoreQuery 0) as score " . $sql, $scoreVals);
+    
+    $count = R::getCell($select . $sql, array_merge($scoreVals, $vals));
+    
     return [
-        "result" => R::getAll($select . $sql, $vals),
+        "result" => $mentors,
         "totalItems" => $count
     ];
-}
+}    
